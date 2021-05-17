a55e06be382b27b50079694ce91ac54e95b617b8
Wayne Rocha <dev.waynerocha@gmail.com>
2021-05-17 18:56:56 -0300

    refactor && style

--- anos.js
+++ anos.js
@@ -1,44 +1,33 @@
-var calendario = document.getElementById('ano');
+const calendario = document.getElementById('ano');
 
 function ApresentarIdade(){
-    let idade = calcularIdade();
-    let localdafoto = document.getElementById('pessoa');
+    const idade = calcularIdade();
+    const localdafoto = document.getElementById('pessoa');
     document.getElementById('idade').innerHTML = "Você tem " + idade + " Anos";
-    if (idade <= 5){
-        //bebê
-        localdafoto.innerHTML =
-        '<img class="imagem__idade" src="img/babys.png" alt="Imagem: Bebês">';
-    }
-    else if (idade > 5 && idade <= 10){
-        //criança
-        localdafoto.innerHTML =
-        '<img class="imagem__idade" src="img/criancas.png" alt="Imagem: Crianças">';
-    }
-    else if(idade > 10 && idade <= 18){
-        //adolecente
-        localdafoto.innerHTML =
-        '<img class="imagem__idade" src="img/adolecentes.png" alt="Imagem: Adolecentes">';
-    }
-    else if(idade > 18 && idade <= 55){
-        //adulto
-        localdafoto.innerHTML =
-        '<img class="imagem__idade" src="img/adultos.png" alt="Imagem: Adultos">';
-    }
-    else{
-        //idoso
-        localdafoto.innerHTML =
-        '<img class="imagem__idade" src="img/idosos.png" alt="Imagem: Idosos">';
-    }
+    const imagemIdade = () => {
+        if (idade <= 5)
+            return '<img class="imagem__idade" src="img/babys.png" alt="Imagem: Bebês">'
+        else if (idade > 5 && idade <= 10)
+            return '<img class="imagem__idade" src="img/criancas.png" alt="Imagem: Crianças">';
+        else if(idade > 10 && idade <= 18)
+            return '<img class="imagem__idade" src="img/adolecentes.png" alt="Imagem: Adolecentes">';
+        else if(idade > 18 && idade <= 55)
+            return '<img class="imagem__idade" src="img/adultos.png" alt="Imagem: Adultos">';
+        else if(idade > 55 && idade <= 110)
+            return '<img class="imagem__idade" src="img/idosos.png" alt="Imagem: Idosos">';
+        else
+            return '<img class="imagem__idade" src="img/dead.webp" alt="Imagem: morto">';
+    };
+    localdafoto.innerHTML = imagemIdade();
 }
 function calcularIdade(){
-    let data = parseInt(calendario.value.slice(0, 4));
-    let idade = Math.abs((data - obterAnoAtual()));
+    const data = parseInt(calendario.value.slice(0, 4));
+    const idade = Math.abs((data - obterAnoAtual()));
     return idade;
 }
 function obterAnoAtual(){
-    let anoAtual = new Date;
-    anoAtual = anoAtual.getFullYear();
-    return anoAtual;
+    const date = new Date;
+    return date.getFullYear();
 }
 
 calendario.addEventListener('change', ApresentarIdade);
